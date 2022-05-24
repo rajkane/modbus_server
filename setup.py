adf55fd136ad35606f87424af083d31e268013ca
Daniel Torac <daniel.torac@gmail.com>
2022-05-24 17:36:45 +0200

    add first init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='modbus_server',
+    version='0.1.0',
+    packages=['src', 'src.view', 'src.model', 'src.controller'],
+    url='',
+    license='MIT',
+    author='Daniel Torac',
+    author_email='daniel.torac@gmail.com',
+    description='GUI modbus tcp'
+)
