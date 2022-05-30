1ee3dd8b85258cd2614ce8d5f391777e87cb5e3b
Daniel Torac <daniel.torac@gmail.com>
2022-05-30 15:19:46 +0200

    modify loggin feature

--- src/model/model_server.py
+++ src/model/model_server.py
@@ -4,9 +4,12 @@ from pyModbusTCP.server import ModbusServer
 from time import sleep
 import logging
 
-logging.basicConfig(filename="server.log", level=logging.DEBUG)
+logging.basicConfig(filename="server.log", filemode="w", level=logging.DEBUG,
+                    format="%(asctime)s\t\t%(levelname)s: %(message)s", datefmt='%Y/%m/%d %H:%M:%S')
 
-NOW = datetime.datetime.now().strftime('%Y/%m/%d - %H:%M:%S')
+
+def date_time_now():
+    return datetime.datetime.now().strftime('%Y/%m/%d - %H:%M:%S')
 
 
 class ServerWorker(qtc.QThread):
@@ -29,10 +32,10 @@ class ServerWorker(qtc.QThread):
             try:
                 self.server = ModbusServer(self.ip, self.port, no_block=True)
                 self.server.start()
-                self.notification.emit(f"{NOW}\tStart server {self.ip}:{self.port} ...")
-                logging.info(f"{NOW}\t\tStart server {self.ip}:{self.port} ...")
-                self.notification.emit(f"{NOW}\tServer is online")
-                logging.info(f"{NOW}\t\tServer is online")
+                self.notification.emit(f"{date_time_now()}\tStart server {self.ip}:{self.port} ...")
+                logging.info(f"Start server {self.ip}:{self.port} ...")
+                self.notification.emit(f"{date_time_now()}\tServer is online")
+                logging.info(f"Server is online")
                 while self.thread_active:
                     if not self.thread_active:
                         break
@@ -40,19 +43,19 @@ class ServerWorker(qtc.QThread):
                     self.status.emit("Server in process")
             except ConnectionError as ce:
                 self.notification.emit(f"{str(ce)}")
-                logging.warning(f"{NOW}\t\t{str(ce)}")
+                logging.warning(f"{self.ip}:{self.port} {str(ce)}")
                 self.stop()
             except OSError as oe:
                 self.notification.emit(f"{str(oe)}")
-                logging.warning(f"{NOW}\t\t{str(oe)}")
+                logging.warning(f"{self.ip}:{self.port} {str(oe)}")
                 self.stop()
 
     def stop(self):
         self.notification.emit(f"Shutdown server ...")
-        logging.info(f"{NOW}\t\tShutdown server ...")
+        logging.info(f"Shutdown server ...")
         self.server.stop()
-        self.notification.emit(f"{NOW}\tServer is offline")
-        logging.info(f"{NOW}\t\tServer is offline")
+        self.notification.emit(f"{date_time_now()}\tServer is offline")
+        logging.info(f"Server is offline")
         self.thread_active = False
         self.enable_process.emit(False)
         self.quit()
